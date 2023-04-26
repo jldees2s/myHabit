f6a75d20e99f2d69b2b1fd823f8f4d5d0a86d678
IAmADwarf <jonathancruzen@gmail.com>
2023-04-26 00:14:08 -0500

    Created MyHabit Project!

---
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/MyHabitApplication.java
@@ -0,0 +1,13 @@
+package com.MyHabit.MyHabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHabitApplication.class, args);
+	}
+
+}
---
+++ MyHabit/MyHabit/src/test/java/com/MyHabit/MyHabit/MyHabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyHabit.MyHabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
