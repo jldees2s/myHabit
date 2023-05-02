168602d546bc6068586a2ab92b0d53ab114b238e
Renzo <larenzostclair@gmail.com>
2023-05-02 11:48:31 -0500

    Added id variable

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
@@ -1,4 +1,13 @@
 package com.MyHabit.MyHabit.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Users {
+
+    @Id @GeneratedValue
+    private int id;
+
 }
