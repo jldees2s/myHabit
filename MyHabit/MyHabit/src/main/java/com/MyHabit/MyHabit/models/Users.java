2133804ec6c4554a4ca9c909e002587abe5d83c9
Renzo <larenzostclair@gmail.com>
2023-05-02 12:19:40 -0500

    Added username and password variables

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
@@ -3,11 +3,20 @@ package com.MyHabit.MyHabit.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Users {
 
     @Id @GeneratedValue
     private int id;
+
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String passwordHash;
+
+
 
 }
