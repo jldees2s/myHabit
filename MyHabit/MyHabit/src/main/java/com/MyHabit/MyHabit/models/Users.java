5a912fbc07df48051752ab145f6805fb8b7fd984
Renzo <larenzostclair@gmail.com>
2023-05-02 13:19:53 -0500

    added constructor, password boolean, and getters and setters

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Users.java
@@ -1,5 +1,7 @@
 package com.MyHabit.MyHabit.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,12 +12,30 @@ public class Users {
 
     @Id @GeneratedValue
     private int id;
-
     @NotNull
     private String userName;
-
     @NotNull
     private String passwordHash;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public Users(){}
+
+    public Users(String uName, String password){
+        this.userName = uName;
+        this.passwordHash = encoder.encode(password);
+    }
+
+    public boolean isMatchingPassword(String password){
+        return encoder.matches(password, passwordHash);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
 
 
