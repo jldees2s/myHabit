201d3be06a7f3712d5fcfa1020110bbeb05ec1e3
Renzo <larenzostclair@gmail.com>
2023-05-03 15:56:25 -0500

    have a response from login page dont have the user saved to the db though

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/AuthenticationController.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/AuthenticationController.java
@@ -1,18 +1,29 @@
 package com.MyHabit.MyHabit.controllers;
 
+import com.MyHabit.MyHabit.models.Habit;
 import com.MyHabit.MyHabit.models.Users;
 import com.MyHabit.MyHabit.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/login")
 public class AuthenticationController {
 
+    @Autowired
     UserRepo userRepo;
 
     private static final String userSessionKey = "user";
+
+    @GetMapping
+    @ResponseBody
+    public String sayHello(){
+        return "Test hello";
+    }
 
     private static void setUserInSession(HttpSession session, Users user){
         session.setAttribute(userSessionKey, user.getId());
@@ -33,4 +44,9 @@ public class AuthenticationController {
         return usersOptional.get();
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public Users addUsers(@RequestBody Users user) {
+        Users newUser = userRepo.save(user);
+        return newUser;
+    }
 }
--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Habit.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/models/Habit.java
@@ -17,4 +17,19 @@ public class Habit {
     @NotBlank
     private String description;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
