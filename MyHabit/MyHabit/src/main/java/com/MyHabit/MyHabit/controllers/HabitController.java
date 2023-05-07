0609c56a19124fa5f02d3f35ab69c3618665dd34
ImADwarf <jonathancruzen@gmail.com>
2023-05-07 12:29:15 -0500

    Added ability to Delete Habits

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/HabitController.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/HabitController.java
@@ -2,9 +2,16 @@ package com.MyHabit.MyHabit.controllers;
 
 import com.MyHabit.MyHabit.models.Habit;
 import com.MyHabit.MyHabit.repositories.HabitRepo;
+import org.hibernate.sql.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/habit")
@@ -23,5 +30,14 @@ public class HabitController {
     public Habit addHabit(@RequestBody Habit habit) {
         Habit newHabit = habitRepo.save(habit);
                 return newHabit;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteHabit(@PathVariable("id") int id) {
+        if(!habitRepo.findById(id).equals(Optional.empty())){
+            habitRepo.deleteById(id);
+            return true;
+        }
+       return false;
     }
 }
