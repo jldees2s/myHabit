eae18203ab0295cd2e83c78c88535548a7b19907
Alexa <obizanzuk@gmail.com>
2023-05-20 18:41:17 -0500

    added / coded homepage component

--- MyHabit/MyHabit/src/main/ui/src/app/app.module.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+// import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +9,13 @@ import { UserloginComponent } from './userlogin/userlogin.component';
 import { HttpClientModule, HttpClient} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 const routes: Routes = [
   {path:"login", component: UserloginComponent},
-  {path:"registration", component: RegistrationComponent}
+  {path:"registration", component: RegistrationComponent},
+  {path:"homepage", component: HomepageComponent}
 
 ]
 
@@ -21,8 +25,10 @@ const routes: Routes = [
     AppComponent,
     UserloginComponent,
     RegistrationComponent,
+    HomepageComponent,
   ],
   imports: [
+    // FlexLayoutModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
