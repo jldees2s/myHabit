d1f8d991ff2998f4201a087ad5e314e845981652
julian <julianschriedel@gmail.com>
2023-05-22 21:41:41 -0500

    created habit settings service class, added methods

---
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/services/HabitSettingsService.java
@@ -0,0 +1,47 @@
+package com.MyHabit.MyHabit.services;
+
+import com.MyHabit.MyHabit.exceptions.UserNotFoundException;
+import com.MyHabit.MyHabit.models.HabitSettings;
+import com.MyHabit.MyHabit.repositories.UserHabitRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HabitSettingsService {
+
+  // import the repo - final, so it cannot be reassigned just values updated
+  private final UserHabitRepo userHabitRepo;
+
+  // CONSTRUCTOR
+  @Autowired
+  public HabitSettingsService(UserHabitRepo userHabitRepo) {
+    this.userHabitRepo = userHabitRepo;
+  }
+
+  // METHODS
+  // add entirely new habit settings
+  public HabitSettings addHabitSettings(HabitSettings habitSettings, boolean active, boolean complete, boolean hidden) {
+    habitSettings.setActive(active);
+    habitSettings.setComplete(complete);
+    habitSettings.setHidden(hidden);
+    return userHabitRepo.save(habitSettings);
+  }
+
+  // update existing habit settings
+  public HabitSettings updateHabitSettings(HabitSettings habitSettings, boolean active, boolean complete, boolean hidden) {
+    habitSettings.setActive(active);
+    habitSettings.setComplete(complete);
+    habitSettings.setHidden(hidden);
+    return userHabitRepo.save(habitSettings);
+  }
+
+  // find habit settings by id
+  public HabitSettings findHabitSettingsById(int id) {
+    return userHabitRepo.findHabitSettingsById(id).orElseThrow(() -> new UserNotFoundException("No HabitSettings Found for HabitSettings ID" + id));
+  }
+
+  // delete habit settings
+  public void deleteHabitSettings(int id) {
+    userHabitRepo.deleteHabitSettingsById(id);
+  }
+}
