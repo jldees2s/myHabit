ee2fadc22f30d47b7653d1ad2b9f5becda7fbd9c
Alexa <obizanzuk@gmail.com>
2023-05-30 14:47:55 -0500

    made default route load to homepage

--- MyHabit/MyHabit/src/main/ui/src/app/app.module.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
+  {path:"", component: HomepageComponent},
   {path:"login", component: UserloginComponent},
   {path:"registration", component: RegistrationComponent},
   {path:"homepage", component: HomepageComponent}
