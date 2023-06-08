730143209f2e470abcfdffcf988b7f401a7f40fc
Alexa <obizanzuk@gmail.com>
2023-06-08 15:25:04 -0500

    finished frontend for createhabit page

---
+++ MyHabit/MyHabit/src/main/ui/src/app/create-habit/create-habit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-habit',
+  templateUrl: './create-habit.component.html',
+  styleUrls: ['./create-habit.component.css']
+})
+export class CreateHabitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
